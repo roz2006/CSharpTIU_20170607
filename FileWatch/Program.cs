5710c0eae50d0c61ddd4f3db1fd25c35b23e22a3
roz2006 <ruoran@gmail.com>
2017-06-07 14:58:27 +0200

    1

---
+++ FileWatch/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileWatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.IO.FileSystemWatcher fsw = new System.IO.FileSystemWatcher(@"c:\temp\", "*.txt");
+            fsw.Changed += Fsw_Changed;
+            fsw.Deleted += (s, e) => { Console.WriteLine("A file {0} is deleted.", e.Name); };
+            fsw.EnableRaisingEvents = true;
+            fsw.Created += (s, e) => { Console.WriteLine("A new file {0} is created.",e.Name); } ;
+            fsw.Created += Fsw_Created1;
+            fsw.NotifyFilter = System.IO.NotifyFilters.FileName | System.IO.NotifyFilters.Size;
+            do
+            {
+
+            } while (true);
+        }
+
+        private static void Fsw_Created1(object sender, System.IO.FileSystemEventArgs e)
+        {
+            Console.WriteLine("A second subscriber");
+        }
+        
+        private static void Fsw_Changed(object sender, System.IO.FileSystemEventArgs e)
+        {
+            Console.WriteLine("Filen {0} er rettet", e.Name);
+        }
+    }
+}
